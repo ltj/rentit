29c4c043cfd176f7dee5a5f937fc63d648578fdb
Kenneth Sohrmann <sohrmann88@hotmail.com>
2012-03-15 16:40:25 +0100

    Changed the name space of the file.

--- RentItServiceLibrary/Database.designer.cs
+++ RentItServiceLibrary/Database.designer.cs
@@ -9,7 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RentItServiceLibrary
+namespace RentItDatabase
 {
     using System.Data.Linq;
     using System.Data.Linq.Mapping;
