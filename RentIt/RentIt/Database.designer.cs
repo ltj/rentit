7659fada6880b36d3d1acbd3896957e50509c273
Splapster <jacrras@gmail.com>
2012-04-15 13:01:27 +0200

    merge conflict.

--- RentIt/RentIt/Database.designer.cs
+++ RentIt/RentIt/Database.designer.cs
@@ -9,7 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RentIt
+namespace RentItDatabase
 {
 	using System.Data.Linq;
 	using System.Data.Linq.Mapping;
