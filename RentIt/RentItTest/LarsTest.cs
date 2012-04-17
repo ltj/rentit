a4aa300769646d93d2f005b126b1d7e892f010b8
Kenneth Sohrmann <sohrmann88@hotmail.com>
2012-04-17 13:17:38 +0200

    Added the RentIt service reference to the test project.

--- RentIt/RentItTest/LarsTest.cs
+++ RentIt/RentItTest/LarsTest.cs
@@ -8,10 +8,13 @@ namespace RentItTest
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
     using System.Text;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using RentIt;
 
     [TestClass]
     public class LarsTest
@@ -19,6 +22,11 @@ namespace RentItTest
         [TestMethod]
         public void TestMethod1()
         {
+            RentItClient client = new RentItClient();
+
+            Account acc = client.ValidateCredentials(new AccountCredentials() { UserName = "publishCorp", HashedPassword = "7110EDA4D09E062AA5E4A390B0A572AC0D2C0220" });
+
+            Assert.AreNotEqual(null, acc);
         }
     }
 }
