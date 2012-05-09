f02b2b6921d700068404df5021ff52c36ef2fa30
Lars Toft Jacobsen <lars@boxed.dk>
2012-05-09 13:34:20 +0200

    MediaFrontpage in progress

--- RentIt/ClientApp/Form1.cs
+++ RentIt/ClientApp/Form1.cs
@@ -6,11 +6,35 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.ServiceModel;
 
 namespace ClientApp {
     public partial class Form1 : Form {
         public Form1() {
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress address = new EndpointAddress("http://rentit.itu.dk/rentit01/RentItService.svc");
+            var rentIt = new RentItClient(binding, address);
             InitializeComponent();
+            mediaFrontpage1.Proxy = rentIt;
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+            mediaFrontpage1.Mtype = RentIt.MediaType.Album;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            switch ((string)comboBox1.SelectedItem) {
+                case "Books":
+                    mediaFrontpage1.Mtype = RentIt.MediaType.Book;
+                    break;
+                case "Music":
+                    mediaFrontpage1.Mtype = RentIt.MediaType.Album;
+                    break;
+                case "Movies":
+                    mediaFrontpage1.Mtype = RentIt.MediaType.Movie;
+                    break;
+            }
+        }
+
     }
 }
---
+++ RentIt/ClientApp/MediaFrontpage.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BinaryCommunicator;
+
+namespace ClientApp {
+    public partial class MediaFrontpage : UserControl {
+
+        private RentIt.MediaType mtype;
+        private RentItClient client;
+        
+        public MediaFrontpage() {
+            InitializeComponent();
+        }
+
+        internal RentIt.MediaType Mtype {
+            get { return mtype; }
+            set { 
+                mtype = value;
+                RefreshContents();
+            }
+        }
+
+        internal RentItClient Proxy {
+            set { client = value; }
+        }
+
+        // Get newest media and publish to "new and hot" area
+        private void GetNewest() {
+            // we need valid prerequisites to query the webservice
+            if(mtype == RentIt.MediaType.Any || client == null) return;
+
+            // build search criteria
+            RentIt.MediaCriteria mc = new RentIt.MediaCriteria {
+                Type = mtype,
+                Limit = 1, // only one result
+                Order = RentIt.MediaOrder.ReleaseDateDesc,
+                Genre = "",
+                SearchText = ""
+            };
+
+            RentIt.MediaItems result = client.GetMediaItems(mc);
+            RentIt.MediaInfo[] subresult = ExtractTypeList(result);
+
+            lblNewTitle.Text = subresult[0].Title;
+            lblNewGenre.Text = subresult[0].Genre;
+            lblNewRelease.Text = subresult[0].ReleaseDate.ToShortDateString();
+            lblNewPublisher.Text = subresult[0].Publisher;
+            lblNewPrice.Text = subresult[0].Price.ToString();
+
+            picNewThumb.Image = BinaryCommuncator.GetThumbnail(subresult[0].Id);
+
+        }
+
+        // Get ten most popular medias
+        private RentIt.MediaItems GetMostPopular() {
+            // we need valid prerequisites to query the webservice
+            if (mtype == RentIt.MediaType.Any || client == null) return null;
+
+            // build search criteria
+            RentIt.MediaCriteria mc = new RentIt.MediaCriteria {
+                Type = mtype,
+                Limit = 10, // only ten most pouplar
+                Order = RentIt.MediaOrder.Default,
+                Genre = "",
+                SearchText = ""
+            };
+
+            RentIt.MediaItems result = client.GetMediaItems(mc);
+            return result;
+        }
+
+        // extrac correct type result from MediaItems return value
+        private RentIt.MediaInfo[] ExtractTypeList(RentIt.MediaItems items) {
+            switch (this.mtype) {
+                case RentIt.MediaType.Album:
+                    return items.Albums;
+                case RentIt.MediaType.Book:
+                    return items.Books;
+                case RentIt.MediaType.Movie:
+                    return items.Movies;
+                default:
+                    return null;
+            }
+        }
+
+        private void RefreshContents() {
+            GetNewest();
+        }
+    }
+}
